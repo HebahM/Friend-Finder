4ca705f5883f0e54c6819fd2adb162a2734e3949
HebahM <56455544+HebahM@users.noreply.github.com>
2020-03-08 16:12:31 -0500

    added front-end. added friends to compare to. added server-side code to add new info to api route

---
+++ app/data/friends.js
@@ -0,0 +1,51 @@
+
+var friends = [
+    {
+        name: "Jake Peralta",
+        photo: "https://vignette.wikia.nocookie.net/brooklynnine-nine/images/7/7f/JakeS5.jpg/revision/latest?cb=20171103222710",
+        scores: [4, 4, 2, 1, 1, 1, 2, 2, 4, 5]
+    },
+    {
+        name: "Amy Santiago",
+        photo: "https://vignette.wikia.nocookie.net/brooklynnine-nine/images/0/06/AmyS5.jpg/revision/latest?cb=20171103223729",
+        scores: [5, 5, 1, 5, 1, 1, 3, 5, 1, 3]
+    },
+    {
+        name: "Rosa Diaz",
+        photo: "https://vignette.wikia.nocookie.net/brooklynnine-nine/images/7/76/RosaS5.jpg/revision/latest?cb=20171103224359",
+        scores: [1, 5, 5, 5, 5, 5, 5, 5, 1, 1]
+    },
+    {
+        name: "Charles Boyle",
+        photo: "https://vignette.wikia.nocookie.net/brooklynnine-nine/images/c/c5/BoyleS5.jpg/revision/latest?cb=20171103224410",
+        scores: [5, 1, 1, 1, 1, 1, 1, 1, 5, 5]
+    },
+    {
+        name: "Raymond Holt",
+        photo: "https://vignette.wikia.nocookie.net/brooklynnine-nine/images/c/ca/HoltS5.jpg/revision/latest?cb=20171103223700",
+        scores: [4, 5, 5, 5, 2, 1, 5, 5, 1, 1]
+    },
+    {
+        name: "Terry Jeffords",
+        photo: "https://vignette.wikia.nocookie.net/brooklynnine-nine/images/f/fd/TerryS5.jpg/revision/latest?cb=20171103223830",
+        scores: [3, 5, 2, 4, 2, 1, 3, 5, 3, 3]
+    },
+    {
+        name: "Gina Linetti",
+        photo: "https://vignette.wikia.nocookie.net/brooklynnine-nine/images/5/53/GinaS5.jpg/revision/latest?cb=20200214190416",
+        scores: [5, 1, 5, 1, 4, 3, 5, 1, 1, 1]
+    },
+    {
+        name: "Scully",
+        photo: "https://vignette.wikia.nocookie.net/brooklynnine-nine/images/6/65/ScullyS5.jpg/revision/latest?cb=20171103224426",
+        scores: [2, 3, 1, 1, 4, 5, 2, 1, 5, 1]
+
+    },
+    {
+        name: "Hitchcock",
+        photo: "https://vignette.wikia.nocookie.net/brooklynnine-nine/images/3/31/HitchcockS5.jpg/revision/latest?cb=20171103224421",
+        scores: [5, 3, 4, 1, 3, 5, 5, 2, 3, 3]
+    }
+];
+
+module.exports = friends;
